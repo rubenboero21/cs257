b9e64d90b3033aa535afde5dfd3c2cd1f78c1cba
Ruben Boero <boeror2017@gmail.com>
2022-09-23 09:36:09 -0500

    Updated .csv file and wrote some tests

--- books/booksdatasourcetests.py
+++ books/booksdatasourcetests.py
@@ -22,10 +22,40 @@ class BooksDataSourceTester(unittest.TestCase):
     def test_all_books(self):
         tiny_data_source = BooksDataSource('tinybooks.csv')
         books = tiny_data_source.books()
+        self.assertTrue(len(books) == 4)
+        self.assertTrue(books[0].title == 'Emma')
+        self.assertTrue(books[1].title == 'Never')
+        self.assertTrue(books[2].title == 'Neverwhere')
+        self.assertTrue(books[3].title == 'Omoo')
+
+    def test_all_books_year(self):
+        tiny_data_source = BooksDataSource('tinybooks.csv')
+        books = tiny_data_source.books('', 'year') # equivalent to books = tiny_data_source.books(sort_by='year')
+        self.assertTrue(len(books) == 4)
+        self.assertTrue(books[0].title == 'Emma')
+        self.assertTrue(books[1].title == 'Omoo')
+        self.assertTrue(books[2].title == 'Never')
+        self.assertTrue(books[3].title == 'Neverwhere')
+
+    def test_search_keyword(self):
+        tiny_data_source = BooksDataSource('tinybooks.csv')
+        books = tiny_data_source.books('Never')
+        self.assertTrue(len(books) == 2)
+        self.assertTrue(books[0].title == 'Never')
+        self.assertTrue(books[1].title == 'Neverwhere')
+
+    def test_search_keyword_year(self):
+        tiny_data_source = BooksDataSource('tinybooks.csv')
+        books = tiny_data_source.books('e', 'year')
         self.assertTrue(len(books) == 3)
         self.assertTrue(books[0].title == 'Emma')
-        self.assertTrue(books[1].title == 'Neverwhere')
-        self.assertTrue(books[2].title == 'Omoo')
+        self.assertTrue(books[1].title == 'Never')
+        self.assertTrue(books[2].title == 'Neverwhere')
+
+    def test_all_authors(self):
+        # come up with a standard for what to do with mulitple authors, write a test to check our standard works
+        # sort books with 2 authors by the author with the surname that comes first alphabetically
+        # the good omens book
 
 if __name__ == '__main__':
     unittest.main()
