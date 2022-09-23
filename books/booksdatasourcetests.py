6c8dd02f055996a14e68cc4ace793c8290836420
Ruben Boero <boeror2017@gmail.com>
2022-09-23 11:13:38 -0500

    Added some tests and fixed tinybooks.csv

--- books/booksdatasourcetests.py
+++ books/booksdatasourcetests.py
@@ -19,6 +19,7 @@ class BooksDataSourceTester(unittest.TestCase):
         self.assertTrue(len(authors) == 1)
         self.assertTrue(authors[0] == Author('Pratchett', 'Terry'))
 
+    # test that default sort works with no search term
     def test_all_books(self):
         tiny_data_source = BooksDataSource('tinybooks.csv')
         books = tiny_data_source.books()
@@ -28,6 +29,7 @@ class BooksDataSourceTester(unittest.TestCase):
         self.assertTrue(books[2].title == 'Neverwhere')
         self.assertTrue(books[3].title == 'Omoo')
 
+    # test that year search works with no search term
     def test_all_books_year(self):
         tiny_data_source = BooksDataSource('tinybooks.csv')
         books = tiny_data_source.books('', 'year') # equivalent to books = tiny_data_source.books(sort_by='year')
@@ -37,6 +39,7 @@ class BooksDataSourceTester(unittest.TestCase):
         self.assertTrue(books[2].title == 'Never')
         self.assertTrue(books[3].title == 'Neverwhere')
 
+    # test that default search works with a search term
     def test_search_keyword(self):
         tiny_data_source = BooksDataSource('tinybooks.csv')
         books = tiny_data_source.books('Never')
@@ -44,6 +47,7 @@ class BooksDataSourceTester(unittest.TestCase):
         self.assertTrue(books[0].title == 'Never')
         self.assertTrue(books[1].title == 'Neverwhere')
 
+    # test that year search works with a search term
     def test_search_keyword_year(self):
         tiny_data_source = BooksDataSource('tinybooks.csv')
         books = tiny_data_source.books('e', 'year')
@@ -51,11 +55,22 @@ class BooksDataSourceTester(unittest.TestCase):
         self.assertTrue(books[0].title == 'Emma')
         self.assertTrue(books[1].title == 'Never')
         self.assertTrue(books[2].title == 'Neverwhere')
+    
+    # add a title search test that inlcudes a book with 2 authors?
 
+    # come up with a standard for what to do with mulitple authors, write a test to check our standard works
+    # sort books with 2 authors by the author with the surname that comes first alphabetically
+    # the good omens book, for example
+
+    # test that author search works with no search term
     def test_all_authors(self):
-        # come up with a standard for what to do with mulitple authors, write a test to check our standard works
-        # sort books with 2 authors by the author with the surname that comes first alphabetically
-        # the good omens book
+        tiny_data_source = BooksDataSource('tinybooks.csv')
+        authors = tiny_data_source.books()
+        self.assertTrue(len(authors) == 4)
+        self.assertTrue(authors[0] == Author('Jane', 'Austen'))
+        self.assertTrue(authors[1] == Author('Neil', 'Gaiman'))
+        self.assertTrue(authors[2] == Author('Herman', 'Melville'))
+        self.assertTrue(authors[3] == Author('Ruben', 'Xiaoying'))
 
 if __name__ == '__main__':
     unittest.main()
