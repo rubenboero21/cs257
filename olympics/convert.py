d0419d78e8822efa8e279ed2ec9cd2462ce39829
Ruben Boero <boeror2017@gmail.com>
2022-10-05 15:15:11 -0500

    Added required files to repo

---
+++ olympics/convert.py
@@ -0,0 +1,4 @@
+# This program was written by Ruben Boero
+
+# The CSV files used in this program can be found on this site:
+# https://www.kaggle.com/heesoo37/120-years-of-olympic-history-athletes-and-results
