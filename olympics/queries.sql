aa07e7d7cc938d9c002b9f6855f01dd21f8faeec
Ruben Boero <boeror2017@gmail.com>
2022-10-09 16:38:41 -0500

    Changed medal queries to be more readable

--- olympics/queries.sql
+++ olympics/queries.sql
@@ -9,20 +9,20 @@ AND noc.id = athletes_noc_olympic_games_events_medals.noc_id
 AND noc.abbreviation = 'JAM'
 ORDER BY name ASC;
 
-SELECT athletes.name, events.event, olympic_games.year, olympic_games.season, medals.class
+SELECT athletes.name, events.event, olympic_games.season, olympic_games.year, medals.class
 FROM athletes, events, olympic_games, medals, athletes_noc_olympic_games_events_medals
 WHERE athletes.name LIKE '%Greg% %Louganis%'
 AND athletes.id = athletes_noc_olympic_games_events_medals.athlete_id
 AND events.id = athletes_noc_olympic_games_events_medals.event_id
 AND olympic_games.id = athletes_noc_olympic_games_events_medals.olympic_game_id
 AND medals.id = athletes_noc_olympic_games_events_medals.medal_id
-AND medals.id != 1
+AND medals.class != 'NA'
 ORDER BY year ASC;
 
 SELECT noc.abbreviation, COUNT(medals.class)
 FROM noc, medals, athletes_noc_olympic_games_events_medals
 WHERE medals.id = athletes_noc_olympic_games_events_medals.medal_id
 AND noc.id = athletes_noc_olympic_games_events_medals.noc_id
-AND medals.id = 4
+AND medals.class = 'Gold'
 GROUP BY noc.abbreviation
 ORDER BY COUNT(medals.class) DESC, noc.abbreviation;
