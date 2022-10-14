685bddeb18802cf0a2606ceb3f5dfb72d1971984
Ruben Boero <boeror2017@gmail.com>
2022-10-14 08:57:05 -0500

    Added case insensitivity for the Greg Louganis query

--- olympics/queries.sql
+++ olympics/queries.sql
@@ -11,7 +11,7 @@ ORDER BY name ASC;
 
 SELECT athletes.name, events.event, olympic_games.season, olympic_games.year, medals.class
 FROM athletes, events, olympic_games, medals, athletes_nocs_olympic_games_events_sports_medals
-WHERE athletes.name LIKE '%Greg% %Louganis%'
+WHERE athletes.name ILIKE '%Greg% %Louganis%'
 AND athletes.id = athletes_nocs_olympic_games_events_sports_medals.athlete_id
 AND events.id = athletes_nocs_olympic_games_events_sports_medals.event_id
 AND olympic_games.id = athletes_nocs_olympic_games_events_sports_medals.olympic_game_id
