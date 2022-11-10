b50e64c1b32d7458f7b093b7622fe614349b1ddc
Serafin Patino <spatino@Serafins-MacBook-Pro.local>
2022-11-09 21:10:22 -0600

    to ruben

--- webapp/static/pokemon.js
+++ webapp/static/pokemon.js
@@ -10,9 +10,14 @@ function initialize() {
     loadGenerationSelector();
     loadLegendariesSelector();
 
-    let element = document.getElementById('generation_selector');
-    if (element) {
-        element.onchange = onGenerationsSelectionChanged;
+    let generation_dropdown = document.getElementById('generation_selector');
+    if (generation_dropdown) {
+        generation_dropdown.onchange = onGenerationsSelectionChanged;
+    }
+
+    let legendary_dropdown = document.getElementById('legendary_selector');
+    if (legendary_dropdown){
+        legendary_dropdown.onchange = onLegendaryCategorySelectionChangedl;
     }
 }
 
@@ -60,39 +65,39 @@ function loadGenerationSelector() {
     });
 }
 
-function loadLegendariesSelector() {
-    let url = getAPIBaseURL() + '/legendaries';
+function onGenerationsSelectionChanged() {
+    let generation_name = this.value; 
+    let url = getAPIBaseURL() + '/generation/' + generation_name;
 
-    // Send the request to the books API /legendaries endpoint
     fetch(url, {method: 'get'})
 
-    // When the results come back, transform them from a JSON string into
-    // a Javascript object.
     .then((response) => response.json())
 
-    // Once you have your list of legendaries, use it to build
-    // an HTML table displaying the legendaries names and.
-    .then(function(legendaries) {
-        // Add the <option> elements to the <select> element
-        let selectorBody = '';
-        // adding a default value to be at the top of the drop down
-        selectorBody += '<option value="' + '--' + '">' + '--' + '</option>\n';
-        for (let k = 0; k < legendaries.length; k++) {
-            let legendary_category = legendaries[k];
-            selectorBody += '<option value="' + legendary_category + '">' + legendary_category + '</option>\n';
-        }
+    .then(function(pokemon_results) {
+    let tableBody = '';
+    // Create the header of the table
+    tableBody += '<tr id = "table_header"><td>Dex Number</td><td>Pokémon</td><td>Ability 1</td><td>Ability 2</td><td>Hidden Ability</td><td>Type 1</td><td>Type 2</td></tr>'
+    // Create the body of the table
+    for (let k = 0; k < pokemon_results.length; k++) {
+    let pokemon = pokemon_results[k];
+    tableBody += '<tr><td>'+ pokemon['dex_num'] + '<td>'+ pokemon['name']+ '</td>' + 
+    '<td>' + pokemon['ability1']+ '</td>' + '<td>' + pokemon['ability2'] + '</td>' + 
+    '<td>' + pokemon['ability3'] + '</td>' + '<td>' + pokemon['type1'] + '</td>' + 
+    '<td>' + pokemon['type2']+ '</td>' + '</td></tr>\n';
+    }
 
-        let selector = document.getElementById('legendary_selector');
-        if (selector) {
-            selector.innerHTML = selectorBody;
-        }
+    // Put the table body we just built inside the table that's already on the page.
+    let generationsTable = document.getElementById('generation_table');
+    if (generationsTable) {
+    generationsTable.innerHTML = tableBody;
+    }
     })
 
-    // Log the error if anything went wrong during the fetch.
     .catch(function(error) {
-        console.log(error);
+    console.log(error);
     });
 }
+
 
 function onLegendaryCategorySelectionChanged() {
     let legendary_category = this.value; 
@@ -128,3 +133,36 @@ function onLegendaryCategorySelectionChanged() {
     });
 }
 
+function loadLegendariesSelector() {
+    let url = getAPIBaseURL() + '/legendaries';
+
+    // Send the request to the books API /legendaries endpoint
+    fetch(url, {method: 'get'})
+
+    // When the results come back, transform them from a JSON string into
+    // a Javascript object.
+    .then((response) => response.json())
+
+    // Once you have your list of legendaries, use it to build
+    // an HTML table displaying the legendaries names and.
+    .then(function(legendaries) {
+        // Add the <option> elements to the <select> element
+        let selectorBody = '';
+        // adding a default value to be at the top of the drop down
+        selectorBody += '<option value="' + '--' + '">' + '--' + '</option>\n';
+        for (let k = 0; k < legendaries.length; k++) {
+            let legendary_category = legendaries[k];
+            selectorBody += '<option value="' + legendary_category + '">' + legendary_category + '</option>\n';
+        }
+
+        let selector = document.getElementById('legendary_selector');
+        if (selector) {
+            selector.innerHTML = selectorBody;
+        }
+    })
+
+    // Log the error if anything went wrong during the fetch.
+    .catch(function(error) {
+        console.log(error);
+    });
+}
