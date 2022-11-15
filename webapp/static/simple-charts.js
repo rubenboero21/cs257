b6e57a2d9c335a975b74583ea0873fb38399660e
Ruben Boero <boeror2017@gmail.com>
2022-11-15 17:16:42 -0600

    Started getting the graphs in the pokemon specific page

---
+++ webapp/static/simple-charts.js
@@ -0,0 +1,50 @@
+/*
+ * simple-charts.js
+ * Jeff Ondich, 12 November 2020
+ *
+ * Adapted from the Chartist library samples.
+ *   https://gionkunz.github.io/chartist-js/examples.html
+ */
+
+window.onload = initialize;
+
+function initialize() {
+    createBarChart();
+}
+
+function createBarChart() {
+    let hp = parseInt(document.getElementById('hp').innerText);
+    let atk = parseInt(document.getElementById('atk').innerText);
+    let def = parseInt(document.getElementById('def').innerText);
+    let spatk = parseInt(document.getElementById('spatk').innerText);
+    let spdef = parseInt(document.getElementById('spdef').innerText);
+    let spd = parseInt(document.getElementById('spd').innerText);
+
+    var data = {
+        labels: ['HP ' + '(' + hp + ')', 'ATK ' + '(' + atk + ')', 'DEF ' + '(' + def + ')', 
+        'SP. ATK ' + '(' + spatk + ')', 'SP. DEF ' + '(' + spdef + ')', 'SPD ' + '(' + spd + ')'],
+
+        series: [[hp, atk, def, spatk, spdef, spd]]
+
+    };
+
+    var options = {horizontalBars: true, reverseData: true,   axisY: {offset: 80}}
+
+    new Chartist.Bar('#stats_chart', data, options);
+
+    // new Chartist.Bar('.ct-chart', {
+    //     labels: ['HP', 'ATK', 'DEF', 'SP. ATK', 'SP. DEF', 'SPD'],
+    //     series: [
+    //       [hp, atk, def, spatk, spdef, spd],
+    //     ]
+    //   }, {
+    //     seriesBarDistance: 10,
+    //     reverseData: true,
+    //     horizontalBars: true,
+    //     // axisY: {
+    //     //   offset: 70
+    //     // }
+    //   });
+}
+
+
