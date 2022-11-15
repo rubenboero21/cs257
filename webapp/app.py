28dea769cd38a34f0a86415f999692c31ba0b0cb
Ruben Boero <boeror2017@gmail.com>
2022-11-15 09:08:57 -0600

    Shotened code and fixed a bug displaying the wrong stats

--- webapp/app.py
+++ webapp/app.py
@@ -29,6 +29,68 @@ def get_connection():
                             user=config.user,
                             password=config.password)
 
+def get_query_results_and_url(query, num_of_inputs, search_text):
+    pokemon_list = []
+    url = []
+
+    try:
+        connection = get_connection()
+        cursor = connection.cursor()
+
+        # the execute line is differente depending on which category is being searched
+        if num_of_inputs == 0:
+            cursor.execute(query,)
+        elif num_of_inputs == 1:
+            cursor.execute(query, (search_text,))
+        elif num_of_inputs == 2:
+            cursor.execute(query, (search_text, search_text,))
+        elif num_of_inputs == 3:
+            cursor.execute(query, (search_text, search_text, search_text,))
+
+        for row in cursor:
+            row = list(row)
+            for i in range(len(row)):
+                row[i] = str(row[i])
+                # give a value of NA to any entry that has no value
+                if row[i] == '':
+                    row[i] = 'NA'
+                # replace any spaces with an underscore (otherwise causes a problem with the url
+                # in jinja -- stops after a space)
+                if ' ' in row[i]:
+                    split = row[i].split(' ')
+                    new_row = ''
+                    for j in range(len(split)):
+                        new_row += split[j]
+                        if (j + 1) < len(split):
+                            new_row += '_'
+                    row[i] = new_row
+
+            pokemon_list.append({'dex_num':row[0], 'name':row[1], 'ability1':row[2], 'ability2':row[3],\
+                'ability3':row[4], 'type1':row[5], 'type2':row[6], 'generation':row[7], 'height':row[8], \
+                'weight':row[9], 'normal_resist':row[10], 'fire_resist':row[11], 'water_resist':row[12], \
+                'electric_resist':row[13], 'grass_resist':row[14], 'ice_resist':row[15], 'fighting_resist':row[16], \
+                'poison_resist':row[17], 'ground_resist':row[18], 'flying_resist':row[19], 'psychic_resist':row[20],\
+                'bug_resist':row[21], 'rock_resist':row[22], 'ghost_resist':row[23], 'dragon_resist':row[24], \
+                'dark_resist':row[25], 'steel_resist':row[26], 'fairy_resist':row[27], 'fairy_resist':row[27], \
+                'hp':row[28], 'atk':row[29], 'def':row[30], 'spatk':row[31], 'spdef':row[31], 'spd':row[32]})
+
+            url.append(
+                row[0] + '/' + row[1] + '/' + row[2] + '/' + row[3] + '/' + row[4] + '/' + row[5] + '/' \
+                + row[6] + '/' + row[7] + '/' + row[8] + '/' + row[9] + '/' + row[10] + '/' + row[11] + '/' \
+                + row[12] + '/' + row[13] + '/' + row[14] + '/' + row[15] + '/' + row[16] + '/' + row[17] + '/' \
+                + row[18] + '/' + row[19] + '/' + row[20] + '/' + row[21] + '/' + row[22] + '/' + row[23] + '/' \
+                + row[24] + '/' + row[25] + '/' + row[26] + '/' + row[27] + '/' + row[28] + '/' + row[29] + '/' \
+                + row[30] + '/' + row[31] + '/' + row[32] + '/' + row[33]
+            )
+                
+        cursor.close()
+        connection.close()
+
+        return pokemon_list, url
+
+    except Exception as e:
+        print(e, file=sys.stderr)
+
 @app.route('/')
 def home():
     return flask.render_template('index.html')
@@ -72,11 +134,6 @@ defense, spatk, spdef, spd):
     dragon_resist=dragon_resist,dark_resist=dark_resist,steel_resist=steel_resist,fairy_resist=fairy_resist, \
     hp=hp, atk=atk, defense=defense, spatk=spatk, spdef=spdef, spd=spd)
 
-# is there a way to shorten the length of this function? 
-# the queries need different number of search inputs, but other than that,
-# theyre the same execute command
-# SOLUTION
-# feed in the number of times the search text is used
 @app.route('/search_results/<category>/<search_text>')
 # def display_search_results():
 def display_search_results(category, search_text):
@@ -157,238 +214,35 @@ def display_search_results(category, search_text):
 
     # if there is no search text, return all pokemon
     if search_text == 'default':
-        try:
-            connection = get_connection()
-            cursor = connection.cursor()
-            #what's the purpose of tuple in jeff's example
-            cursor.execute(query,)
-            for row in cursor:
-                row = list(row)
-                # give a value of None to any entry that has no value
-                for i in range(len(row)):
-                    row[i] = str(row[i])
-                    if row[i] == '':
-                        row[i] = 'NA'
-                    # in order for the link to work properly when passed into the html, all 
-                    # portions of the link must be a single string (generation 1 breaks it
-                    # it must be generation_1, for example)
-                    if 'generation' in row[i]:
-                        split = row[i].split(' ')
-                        row[i] = split[0] + '_' + split[1]
-                    # in order for the link to work when passed into the html, it cant have
-                    # spaces in it, so replace spaces with an underscore
-                    if ' ' in row[i]:
-                        split = row[i].split(' ')
-                        new_row = ''
-                        for j in range(len(split)):
-                            new_row += split[j]
-                            if (j + 1) < len(split):
-                                new_row += '_'
-                        row[i] = new_row
+        result = get_query_results_and_url(query, 0, '')
+        pokemon_list = result[0]
+        url = result[1]
 
-                pokemon_list.append({'dex_num':row[0], 'name':row[1], 'ability1':row[2], 'ability2':row[3],\
-                    'ability3':row[4], 'type1':row[5], 'type2':row[6], 'generation':row[7], 'height':row[8], \
-                    'weight':row[9], 'normal_resist':row[10], 'fire_resist':row[11], 'water_resist':row[12], \
-                    'electric_resist':row[13], 'grass_resist':row[14], 'ice_resist':row[15], 'fighting_resist':row[16], \
-                    'poison_resist':row[17], 'ground_resist':row[18], 'flying_resist':row[19], 'psychic_resist':row[20],\
-                    'bug_resist':row[21], 'rock_resist':row[22], 'ghost_resist':row[23], 'dragon_resist':row[24], \
-                    'dark_resist':row[25], 'steel_resist':row[26], 'fairy_resist':row[27], 'fairy_resist':row[27], \
-                    'hp':row[28], 'atk':row[29], 'def':row[30], 'spatk':row[31], 'spdef':row[31], 'spd':row[32]})
-
-                url.append(
-                    row[0] + '/' + row[1] + '/' + row[2] + '/' + row[3] + '/' + row[4] + '/' + row[5] + '/' \
-                    + row[6] + '/' + row[7] + '/' + row[8] + '/' + row[9] + '/' + row[10] + '/' + row[11] + '/' \
-                    + row[12] + '/' + row[13] + '/' + row[14] + '/' + row[15] + '/' + row[16] + '/' + row[17] + '/' \
-                    + row[18] + '/' + row[19] + '/' + row[20] + '/' + row[21] + '/' + row[22] + '/' + row[23] + '/' \
-                    + row[24] + '/' + row[25] + '/' + row[26] + '/' + row[27] + '/' + row[28] + '/' + row[29] + '/' \
-                    + row[30] + '/' + row[31] + '/' + row[31] + '/' + row[32]
-                )
-                    
-            cursor.close()
-            connection.close()
-
-        except Exception as e:
-            print(e, file=sys.stderr)
-
-        # return the list of dictionaries to the html, then parse it inside HTML
         return flask.render_template('search_results.html', search_results=pokemon_list, url=url)
 
     elif category == 'pokemon' or category == 'pokedex_number':
-        try:
-            connection = get_connection()
-            cursor = connection.cursor()
-            #what's the purpose of tuple in jeff's example
-            cursor.execute(query, (search_text,))
-            for row in cursor:
-                row = list(row)
-                # give a value of None to any entry that has no value
-                for i in range(len(row)):
-                    row[i] = str(row[i])
-                    if row[i] == '':
-                        row[i] = 'NA'
-                    # in order for the link to work properly when passed into the html, all 
-                    # portions of the link must be a single string (generation 1 breaks it
-                    # it must be generation_1, for example)
-                    if 'generation' in row[i]:
-                        split = row[i].split(' ')
-                        row[i] = split[0] + '_' + split[1]
-                    # in order for the link to work when passed into the html, it cant have
-                    # spaces in it, so replace spaces with an underscore
-                    if ' ' in row[i]:
-                        split = row[i].split(' ')
-                        new_row = ''
-                        for j in range(len(split)):
-                            new_row += split[j]
-                            if (j + 1) < len(split):
-                                new_row += '_'
-                        row[i] = new_row
-
-                pokemon_list.append({'dex_num':row[0], 'name':row[1], 'ability1':row[2], 'ability2':row[3],\
-                    'ability3':row[4], 'type1':row[5], 'type2':row[6], 'generation':row[7], 'height':row[8], \
-                    'weight':row[9], 'normal_resist':row[10], 'fire_resist':row[11], 'water_resist':row[12], \
-                    'electric_resist':row[13], 'grass_resist':row[14], 'ice_resist':row[15], 'fighting_resist':row[16], \
-                    'poison_resist':row[17], 'ground_resist':row[18], 'flying_resist':row[19], 'psychic_resist':row[20],\
-                    'bug_resist':row[21], 'rock_resist':row[22], 'ghost_resist':row[23], 'dragon_resist':row[24], \
-                    'dark_resist':row[25], 'steel_resist':row[26], 'fairy_resist':row[27], 'fairy_resist':row[27], \
-                    'hp':row[28], 'atk':row[29], 'def':row[30], 'spatk':row[31], 'spdef':row[31], 'spd':row[32]})
-
-                url.append(
-                    row[0] + '/' + row[1] + '/' + row[2] + '/' + row[3] + '/' + row[4] + '/' + row[5] + '/' \
-                    + row[6] + '/' + row[7] + '/' + row[8] + '/' + row[9] + '/' + row[10] + '/' + row[11] + '/' \
-                    + row[12] + '/' + row[13] + '/' + row[14] + '/' + row[15] + '/' + row[16] + '/' + row[17] + '/' \
-                    + row[18] + '/' + row[19] + '/' + row[20] + '/' + row[21] + '/' + row[22] + '/' + row[23] + '/' \
-                    + row[24] + '/' + row[25] + '/' + row[26] + '/' + row[27] + '/' + row[28] + '/' + row[29] + '/' \
-                    + row[30] + '/' + row[31] + '/' + row[31] + '/' + row[32]
-                )
-                    
-            cursor.close()
-            connection.close()
-
-        except Exception as e:
-            print(e, file=sys.stderr)
+        result = get_query_results_and_url(query, 1, search_text)
+        pokemon_list = result[0]
+        url = result[1]
 
         # return the list of dictionaries to the html, then parse it inside HTML
         return flask.render_template('search_results.html', search_results=pokemon_list, url=url)
     
     elif category == 'ability':
-        try:
-            connection = get_connection()
-            cursor = connection.cursor()
-            #what's the purpose of tuple in jeff's example
-            cursor.execute(query, (search_text, search_text, search_text,))
-            for row in cursor:
-                row = list(row)
-                # give a value of None to any entry that has no value
-                for i in range(len(row)):
-                    row[i] = str(row[i])
-                    if row[i] == '':
-                        row[i] = 'NA'
-                    # in order for the link to work properly when passed into the html, all 
-                    # portions of the link must be a single string (generation 1 breaks it
-                    # it must be generation_1, for example)
-                    if 'generation' in row[i]:
-                        split = row[i].split(' ')
-                        row[i] = split[0] + '_' + split[1]
-                    # in order for the link to work when passed into the html, it cant have
-                    # spaces in it, so replace spaces with an underscore
-                    if ' ' in row[i]:
-                        split = row[i].split(' ')
-                        new_row = ''
-                        for j in range(len(split)):
-                            new_row += split[j]
-                            if (j + 1) < len(split):
-                                new_row += '_'
-                        row[i] = new_row
-
-                pokemon_list.append({'dex_num':row[0], 'name':row[1], 'ability1':row[2], 'ability2':row[3],\
-                    'ability3':row[4], 'type1':row[5], 'type2':row[6], 'generation':row[7], 'height':row[8], \
-                    'weight':row[9], 'normal_resist':row[10], 'fire_resist':row[11], 'water_resist':row[12], \
-                    'electric_resist':row[13], 'grass_resist':row[14], 'ice_resist':row[15], 'fighting_resist':row[16], \
-                    'poison_resist':row[17], 'ground_resist':row[18], 'flying_resist':row[19], 'psychic_resist':row[20],\
-                    'bug_resist':row[21], 'rock_resist':row[22], 'ghost_resist':row[23], 'dragon_resist':row[24], \
-                    'dark_resist':row[25], 'steel_resist':row[26], 'fairy_resist':row[27], 'fairy_resist':row[27], \
-                    'hp':row[28], 'atk':row[29], 'def':row[30], 'spatk':row[31], 'spdef':row[31], 'spd':row[32]})
-
-                url.append(
-                    row[0] + '/' + row[1] + '/' + row[2] + '/' + row[3] + '/' + row[4] + '/' + row[5] + '/' \
-                    + row[6] + '/' + row[7] + '/' + row[8] + '/' + row[9] + '/' + row[10] + '/' + row[11] + '/' \
-                    + row[12] + '/' + row[13] + '/' + row[14] + '/' + row[15] + '/' + row[16] + '/' + row[17] + '/' \
-                    + row[18] + '/' + row[19] + '/' + row[20] + '/' + row[21] + '/' + row[22] + '/' + row[23] + '/' \
-                    + row[24] + '/' + row[25] + '/' + row[26] + '/' + row[27] + '/' + row[28] + '/' + row[29] + '/' \
-                    + row[30] + '/' + row[31] + '/' + row[31] + '/' + row[32]
-                )
-                    
-            cursor.close()
-            connection.close()
-            
-        except Exception as e:
-            print(e, file=sys.stderr)
+        result = get_query_results_and_url(query, 3, search_text)
+        pokemon_list = result[0]
+        url = result[1]
 
         # return the list of dictionaries to the html, then parse it inside HTML
         return flask.render_template('search_results.html', search_results=pokemon_list, url=url)
     
     elif category == 'type':
-        try:
-            connection = get_connection()
-            cursor = connection.cursor()
-            #what's the purpose of tuple in jeff's example
-            cursor.execute(query, (search_text, search_text,))
-            for row in cursor:
-                row = list(row)
-                # give a value of None to any entry that has no value
-                for i in range(len(row)):
-                    row[i] = str(row[i])
-                    if row[i] == '':
-                        row[i] = 'NA'
-                    # in order for the link to work properly when passed into the html, all 
-                    # portions of the link must be a single string (generation 1 breaks it
-                    # it must be generation_1, for example)
-                    if 'generation' in row[i]:
-                        split = row[i].split(' ')
-                        row[i] = split[0] + '_' + split[1]
-                    # in order for the link to work when passed into the html, it cant have
-                    # spaces in it, so replace spaces with an underscore
-                    if ' ' in row[i]:
-                        split = row[i].split(' ')
-                        new_row = ''
-                        for j in range(len(split)):
-                            new_row += split[j]
-                            if (j + 1) < len(split):
-                                new_row += '_'
-                        row[i] = new_row
-
-                pokemon_list.append({'dex_num':row[0], 'name':row[1], 'ability1':row[2], 'ability2':row[3],\
-                    'ability3':row[4], 'type1':row[5], 'type2':row[6], 'generation':row[7], 'height':row[8], \
-                    'weight':row[9], 'normal_resist':row[10], 'fire_resist':row[11], 'water_resist':row[12], \
-                    'electric_resist':row[13], 'grass_resist':row[14], 'ice_resist':row[15], 'fighting_resist':row[16], \
-                    'poison_resist':row[17], 'ground_resist':row[18], 'flying_resist':row[19], 'psychic_resist':row[20],\
-                    'bug_resist':row[21], 'rock_resist':row[22], 'ghost_resist':row[23], 'dragon_resist':row[24], \
-                    'dark_resist':row[25], 'steel_resist':row[26], 'fairy_resist':row[27], 'fairy_resist':row[27], \
-                    'hp':row[28], 'atk':row[29], 'def':row[30], 'spatk':row[31], 'spdef':row[31], 'spd':row[32]})
-
-                url.append(
-                    row[0] + '/' + row[1] + '/' + row[2] + '/' + row[3] + '/' + row[4] + '/' + row[5] + '/' \
-                    + row[6] + '/' + row[7] + '/' + row[8] + '/' + row[9] + '/' + row[10] + '/' + row[11] + '/' \
-                    + row[12] + '/' + row[13] + '/' + row[14] + '/' + row[15] + '/' + row[16] + '/' + row[17] + '/' \
-                    + row[18] + '/' + row[19] + '/' + row[20] + '/' + row[21] + '/' + row[22] + '/' + row[23] + '/' \
-                    + row[24] + '/' + row[25] + '/' + row[26] + '/' + row[27] + '/' + row[28] + '/' + row[29] + '/' \
-                    + row[30] + '/' + row[31] + '/' + row[31] + '/' + row[32]
-                )
-                    
-            cursor.close()
-            connection.close()
-            
-        except Exception as e:
-            print(e, file=sys.stderr)
+        result = get_query_results_and_url(query, 2, search_text)
+        pokemon_list = result[0]
+        url = result[1]
 
         # return the list of dictionaries to the html, then parse it inside HTML
         return flask.render_template('search_results.html', search_results=pokemon_list, url=url)
-
-# send in the info needed for the pokemon specific page in the url. construct the URL in the JS after
-# the JSON dump. then this route can parse out each piece of information
-# @app.route('/pokemon/<ID-dex_num-name-etc.>')
-# def pokemon_detail(useful variables):
-#     return flask.render_template(same useful variables)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser('A Pokémon search engine/database')
